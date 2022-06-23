67f52f91fda8079207076bdbc93433733fc65fbc
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-23 22:32:59 +0530

    fggf

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./fonts/zai-TributeToCaselli-Pantelegraph.ttf";
 import Index from "./routes/index"
 function App() {
   return (
-    <div className=''>
-     <Index />
+    <div className='app'>
+    <h1>React Router ships with a few hooks that let you access the state of the router and perform navigation from inside your components.</h1>
     </div>
   );
 }
