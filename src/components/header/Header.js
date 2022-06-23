1b2e4032462fd1c0bcf274014e8ef0cca4ece1b5
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-24 00:48:52 +0530

    dddd

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,7 @@
 import React, {  } from "react";
-import { Link } from "react-router-dom";
+
 import './header.css'
-import Brand from '../../assets/images/home/logo.png'
-import Nav_1 from '../../assets/images/home/nav-1.png'
-import Nav_2 from '../../assets/images/home/nav-2.png'
+
 
 
 
