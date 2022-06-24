bad6d1ec690c86f242dac314fde65572c89bc2d1
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-25 02:11:13 +0530

    vvvvv

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,8 @@
-import React,{} from "react";
-import './home.css'
-
+import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-    <section class="text-[#01fdf0] body-font text-center">
-
-    <h1 className='text-center text-yellow-500'>Homes Page</h1>
-
- 
-    </section>
-    </div>
+    <div>Home</div>
   )
 }
 
