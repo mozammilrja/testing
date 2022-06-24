e746c9622b5a9c6b972ebe3b20634893b04d48ec
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-25 01:03:33 +0530

    ttt

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,12 +12,6 @@ const Home = () => {
     <h1 className='bug-bounty-heading'>WEB 3.0 BUG BOUNTY PLATFORM</h1>
 
 <h3 className='bug-bounty-sub-heading'>Secure, Transparent, Rewarding!</h3>
-
-<p className='text-xl pt-4  text-[#f1c40f]' style={{padding:'0 10%'}}>Bug Bounty Platform to get rid of vulnerabilities before anyone even knows they exist.</p>
-
-<p  className='text-base pt-4 text-[#f1c40f]'>
-Cybercrime is relentless and unlikely to stop. With our Bug Bounty Platform get yourself a professional triage team, ongoing crowdsourced security programs, the whitehat army, a secure dashboard, and more for your DeFi and crypto projects
-  </p>
    </div>
     </section>
     </div>
