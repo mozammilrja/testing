66b5156b03c518ff7d24a3fcd846e1137c565257
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-24 22:21:36 +0530

    dddd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
+
 import './App.css';
+import { Button } from 'react-bootstrap';
 
-import "./fonts/zai-TributeToCaselli-Pantelegraph.ttf";
-import RouteComponent from "./routes/RouteComponent"
-
-const App = () => {
+function App() {
   return (
-    <div>
-    <RouteComponent/>
+    <div className="">
+    <Button variant="primary">Primary</Button>{' '}
+    <Button variant="secondary">Secondary</Button>{' '}
+    <Button variant="success">Success</Button>{' '}
+    <Button variant="warning">Warning</Button>{' '}
+    <Button variant="danger">Danger</Button>{' '}
+    <Button variant="info">Info</Button>{' '}
+    <Button variant="light">Light</Button>{' '}
+    <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    fontFamily: {
-      display: ["Open Sans", "sans-serif"],
-      body: ["Open Sans", "sans-serif"],
-    },
+    extend: {},
   },
-  extend: {
-    backgroundImage: {
-      'hero-pattern': "url('https://img.freepik.com/free-vector/abstract-geometric-pattern-background_1319-242.jpg?t=st=1655760437~exp=1655761037~hmac=6de5d9bc7d85c6fa12673191f87354bdc8a6435e9b5ec8cad9abd00ff60e2c6c&w=900')",
-      'footer-texture': "url('')",
-    }
-  },
-  plugins: [require('daisyui')],
-
+  plugins: [],
 }
