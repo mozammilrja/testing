7029a0dfe059066388e52013ba24806b7e29d291
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-25 08:40:44 +0530

    nnn

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React,{} from "react";
+import './home.css'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    <section class="text-[#01fdf0] body-font text-center">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <h1 className='text-center text-yellow-500'>Homes Page</h1>
+    <div class="container px-5 py-24 mx-auto">
+    <h1 className='bug-bounty-heading'>WEB 3.0 BUG BOUNTY PLATFORM</h1>
+    <h3 className='bug-bounty-sub-heading'>Secure, Transparent, Rewarding!</h3>
+   </div>
+    </section>
+    </div>
   )
 }
 
