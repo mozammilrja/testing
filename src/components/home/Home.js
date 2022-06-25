39d99c1b4f7a38462c885a08a3dee8b11c9e2217
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-06-25 08:43:02 +0530

    nmmn

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,10 +6,6 @@ const Home = () => {
   return (
     <div>
     <section class="text-[#01fdf0] body-font text-center">
-    <br/>
-    <br/>
-    <br/>
-    <br/>
     <h1 className='text-center text-yellow-500'>Homes Page</h1>
     <div class="container px-5 py-24 mx-auto">
     <h1 className='bug-bounty-heading'>WEB 3.0 BUG BOUNTY PLATFORM</h1>
